8396379c2b42688d8baceae5d8e36ef21b3da24a
Lester1989 <lester@techfak.uni-bielefeld.de>
2024-09-03 23:38:22 +0200

    initial and hopefully final commit

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+uploaded_coordinates: list[tuple[float, float]] = []
+
+secret_key = os.environ.get("SECRET_KEY", None)
+
+@app.post("/upload_coordinates")
+async def upload_coordinates(request: Request,coordinates: list[tuple[float, float]]):
+    auth = request.headers.get("Authorization")
+    if auth != f"Bearer {secret_key}":
+        return {"message": "Invalid key"}
+    uploaded_coordinates.clear()
+    uploaded_coordinates.extend(coordinates)
+    return {"message": "Coordinates uploaded successfully"}
+
+
+@app.get("/gothic_tales_coordinates")
+async def render_template(request: Request):
+    return templates.TemplateResponse(
+        "map.html", {"request": request, "coordinates": uploaded_coordinates}
+    )
